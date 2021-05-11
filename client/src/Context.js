74616a694dcc32040cb2e31081715d47e0aee2f9
VibhuGautam <vibhuparmar13@gmail.com>
2021-05-11 13:02:50 +0530

    Deployed  Site

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('https://vanilla-video-chat.netlify.app');
+const socket = io('https://vanilla-chat-13.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
